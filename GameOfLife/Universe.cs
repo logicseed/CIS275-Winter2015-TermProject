c824234a3553177e50d38b3eceeb5aa0c20e7e0e
logicseed <mjking@umich.edu>
2015-01-18 15:25:32 -0500

    Initial Setup

---
+++ GameOfLife/Universe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Universe
+    {
+        private int[][] CurrentMatrix;
+        private int[][] NewMatrix;
+
+        private void NextGeneration()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Universe()
+        {
+
+        }
+    }
+}
