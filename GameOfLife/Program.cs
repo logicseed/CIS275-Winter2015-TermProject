a1516791039208854ef7ef5c30f60542807fa939
logicseed <mjking@umich.edu>
2015-01-21 10:25:00 -0500

    GridSize and GridPosition; created properties and switch to Size and PointF.

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -16,12 +16,24 @@ namespace GameOfLife
 {
     public partial class Program : Form
     {
+        // Program-state fields
         private bool isAutoStepping = false;
         private bool hasShownSplash = false;
 
+        // Additional program-state fields that monitor the state
+        // of certain fields that require calculations. The calculations
+        // are only performed if the they are considered invalid due to
+        // other program changes.
+        // Example: Once the game starts, the grid cannot be resized so
+        // it is safe to use the last known GridSize and GridPosition.
+        private bool isGridSizeValid = false;
+        private bool isGridPositionValid = false;
+
+        // Matrix fields
         private byte rows = 20;
         private byte columns = 30;
         private byte cellSize = 20;
+        private byte lifeChance = 50;
 
         private const int SPLASH_HEIGHT = 600;
         private const int SPLASH_WIDTH = 800;
@@ -29,6 +41,67 @@ namespace GameOfLife
         private LifeManager lifeManager = new LifeManager();
 
         private PrivateFontCollection customFonts = new PrivateFontCollection();
+
+        // The screen buffer is used to build the screen before displaying it.
+        private Bitmap screenBuffer;
+
+        private Size gridSize;
+        private PointF gridPosition;
+
+        private Size GridSize
+        {
+            get
+            {
+                if(!isGridSizeValid)
+                {
+                    // Calculate the size of the grid.
+                    // We want a pixel between each cell and a pixel surrounding
+                    // all the cells.
+                    gridSize = new Size(
+                        (cellSize * columns) + columns, // Width
+                        (cellSize * rows) + rows // Height
+                    );
+
+                    // Claim gridSize is valid.
+                    isGridSizeValid = true;
+
+                    return gridSize;
+                }
+                else
+                {
+                    return gridSize;
+                }
+            }
+        }
+
+        private PointF GridPosition
+        {
+            get
+            {
+                if(!isGridPositionValid)
+                {
+                    // The grid position is based on the size of the screen.
+                    // We attempt to center the grid vertically and horizontally.
+                    // To overcome grid flickering due to anti-aliasing, we add
+                    // 0.5f to each value; see Book2 page 32 for explanation.
+                    gridPosition = new PointF(
+                        (float)Math.Floor((this.Width - GridSize.Width) / 2.0f) + 0.5f,
+                        (float)Math.Floor((this.Height - GridSize.Height) / 2.0f) + 0.5f
+                    );
+
+                    // Claim gridPosition is valid.
+                    isGridPositionValid = true;
+
+                    return gridPosition;
+                }
+                else
+                {
+                    return gridPosition;
+                }
+            }
+        }
+
+        
 
         public Program()
         {
@@ -43,12 +116,24 @@ namespace GameOfLife
             customFonts.AddFontFile("GameOfLife-Regular.ttf");
             customFonts.AddFontFile("GameOfLife-Bold.ttf");
 
+            // Initialize the screen buffer bitmap.
+            screenBuffer = new Bitmap(this.Width, this.Height);
+
         }
 
         
 
         private void Program_Paint(object sender, PaintEventArgs e)
         {
+
+            // Have everything draw into a buffer and return it.
+            // This should prevent the flickering and also make it so 
+            // we don't have to send the arguments along for the trip.
+
+            
+
+
+
             if(!hasShownSplash)
             {
                 DrawSplashScreen(sender, e);
@@ -61,6 +146,7 @@ namespace GameOfLife
 
         private void DrawSplashScreen(object sender, PaintEventArgs e)
         {
+            //Graphics g = Graphics.FromImage(screenBuffer);
             Graphics g = e.Graphics;
 
             // Draw background.
@@ -104,41 +190,28 @@ namespace GameOfLife
         {
             Graphics g = e.Graphics;
 
-            // Calculate the size of the grid.
-            // We want a pixel between each cell and a pixel surrounding
-            // all the cells.
-            int gridWidth = (cellSize * columns) // Space for life
-                + columns; // Space for borders
-            int gridHeight = (cellSize * rows) // Space for life
-                + rows; // Space for borders
-
-            // Calculate grid position.
-            Point gridPosition = new Point(
-                (this.Width - gridWidth)/2,
-                (this.Height - gridHeight)/2);
-
             // Draw surrounding rectangle.
-            e.Graphics.DrawRectangle(cellPen, gridPosition.X, gridPosition.Y,
-                gridWidth, gridHeight);
+            e.Graphics.DrawRectangle(cellPen, GridPosition.X, GridPosition.Y,
+                GridSize.Width, GridSize.Height);
 
             // Draw horizontal lines.
             for(int i = 1; i < rows; i++)
             {
                 e.Graphics.DrawLine(cellPen, 
-                    gridPosition.X,
-                    gridPosition.Y + i + (i * cellSize),
-                    gridPosition.X + gridWidth,
-                    gridPosition.Y + i + (i * cellSize));
+                    GridPosition.X,
+                    GridPosition.Y + i + (i * cellSize),
+                    GridPosition.X + GridSize.Width,
+                    GridPosition.Y + i + (i * cellSize));
             }
 
             // Draw vertical lines.
             for (int i = 1; i < columns; i++)
             {
                 e.Graphics.DrawLine(cellPen, 
-                    gridPosition.X + i + (i * cellSize),
-                    gridPosition.Y,
-                    gridPosition.X + i + (i * cellSize),
-                    gridPosition.Y + gridHeight);
+                    GridPosition.X + i + (i * cellSize),
+                    GridPosition.Y,
+                    GridPosition.X + i + (i * cellSize),
+                    GridPosition.Y + GridSize.Height);
             }
         }
 
@@ -146,33 +219,26 @@ namespace GameOfLife
         {
             Graphics g = e.Graphics;
 
-            // Calculate the size of the grid.
-            // We want a pixel between each cell and a pixel surrounding
-            // all the cells.
-            int gridWidth = (cellSize * columns) // Space for life
-                + columns; // Space for borders
-            int gridHeight = (cellSize * rows) // Space for life
-                + rows; // Space for borders
-
-            // Calculate grid position.
-            Point gridPosition = new Point(
-                (this.Width - gridWidth) / 2,
-                (this.Height - gridHeight) / 2);
-
             // Draw life rectangles
-            for(int i = 0; i < rows; i++)
+            for(int i = 1; i <= rows; i++)
             {
-                for(int j = 0; j < columns; j++)
+                for(int j = 1; j <= columns; j++)
                 {
-                    if (lifeManager.GetElement(i+1, j+1) > 0)
+                    if (lifeManager.GetElement(i, j) > 0)
                     {
-                        g.FillRectangle(lifeBrush[lifeManager.GetElement(i+1, j+1)],
-                            gridPosition.X + 1 + ((cellSize+1) * j),
-                            gridPosition.Y + 1 + ((cellSize+1) * i),
+                        g.FillRectangle(lifeBrush[lifeManager.GetElement(i, j)],
+                            GridPosition.X + ((cellSize+1) * (j-1)),
+                            GridPosition.Y + ((cellSize+1) * (i-1)),
                             cellSize, cellSize);
                     }
                 }
             }
+        }
+
+        private void InvalidateGrid()
+        {
+            isGridSizeValid = false;
+            isGridPositionValid = false;
         }
     }
 }
